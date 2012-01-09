db08a97b1410a17ec7731ea7ff22f603ecbee7ef
Omari Norman <omari@smileystation.com>
2012-01-08 20:10:04 -0500

    new sample parser

--- System/Console/MultiArg/SampleParser.hs
+++ System/Console/MultiArg/SampleParser.hs
@@ -7,19 +7,19 @@ module System.Console.MultiArg.SampleParser where
 
 import System.Console.MultiArg
 
-specs :: [OptSpec String]
+specs :: [OptSpec]
 
-specs = [ OptSpec "bytes"        "c" ["bytes"]                 SOneArg
-        , OptSpec "follow"       "f" ["follow"]                SOptionalArg
-        , OptSpec "follow-retry" "F" []                        SNoArg
-        , OptSpec "lines"        "n" ["lines"]                 SOneArg
-        , OptSpec "states"       ""  ["max-unchanged-stats"]   SOneArg
-        , OptSpec "pid"          ""  ["pid"]                   SOneArg
-        , OptSpec "quiet"        "q" ["quiet", "silent"]       SNoArg
-        , OptSpec "sleep"        "s" ["sleep-interval"]        SOneArg
-        , OptSpec "verbose"      "v" ["verbose"]               SNoArg
-        , OptSpec "help"         ""  ["help"]                  SNoArg
-        , OptSpec "version"      ""  ["version"]               SNoArg
+specs = [ OptSpec "bytes"               "c" []          SOneArg
+        , OptSpec "follow"              "f" []          SOptionalArg
+        , OptSpec "follow-retry"        "F" []          SNoArg
+        , OptSpec "lines"               "n" []          SOneArg
+        , OptSpec "max-unchanged-stats" ""  []          SOneArg
+        , OptSpec "pid"                 ""  []          SOneArg
+        , OptSpec "quiet"               "q" ["silent"]  SNoArg
+        , OptSpec "sleep-interval"      "s" []          SOneArg
+        , OptSpec "verbose"             "v" []          SNoArg
+        , OptSpec "help"                ""  []          SNoArg
+        , OptSpec "version"             ""  []          SNoArg
         ]
 
 main :: IO ()
